021705a387f9d67f3a6ea91d93b38a61ef8849e1
Attila Bán <144130755+attilaxban@users.noreply.github.com>
2024-09-09 16:00:36 +0200

    Update tv.route.js

--- backend/Routes/tv.route.js
+++ backend/Routes/tv.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getSimilarTvs, getTrendingTv, getTvDetails, getTvsByCategory, getTvTrailers, } from '../Controllers/tv.controller.js';
+import { getSimilarTvs, getTrendingTv, getTvDetails, getTvsByCategory, getTvTrailers, getTvsByGenres} from '../Controllers/tv.controller.js';
 import { verifyToken } from "../config/tokenGenerator.js";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get("/:id/trailers", verifyToken, getTvTrailers);
 router.get("/:id/details", verifyToken, getTvDetails);
 router.get("/:id/similar", verifyToken, getSimilarTvs);
 router.get("/:category", verifyToken, getTvsByCategory);
+router.get("/genre/:genreID", verifyToken, getTvsByGenres)
 
 export default router;
