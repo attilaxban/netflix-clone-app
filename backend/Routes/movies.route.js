da2baeaf861a522f0cec38d031c87e4c65c066af
Attila Bán <144130755+attilaxban@users.noreply.github.com>
2024-09-09 16:01:27 +0200

    Update movies.route.js

--- backend/Routes/movies.route.js
+++ backend/Routes/movies.route.js
@@ -1,10 +1,6 @@
 import express from 'express';
-
-<<<<<<< HEAD
-import { getTrendingMovie, getMovieTrailers, getMovieDetails, getSimilarMovies, getMoviesByCategory, getMoviesByGenre } from '../Controllers/movies.controller.js';
-=======
 import { getTrendingMovie, getMovieTrailers, getMovieDetails, getSimilarMovies, getMoviesByCategory,getMoviesByGenres } from '../Controllers/movies.controller.js';
->>>>>>> frontend
+
 import { verifyToken } from '../config/tokenGenerator.js';
 
 
@@ -15,10 +11,6 @@ router.get('/:id/trailers', verifyToken, getMovieTrailers)
 router.get('/:id/details', verifyToken, getMovieDetails)
 router.get('/:id/similar', verifyToken, getSimilarMovies)
 router.get('/:category', verifyToken, getMoviesByCategory)
-<<<<<<< HEAD
-router.get('/genre/:genreID', verifyToken, getMoviesByGenre)
-=======
 router.get('/genre/:genreID', verifyToken, getMoviesByGenres)
->>>>>>> frontend
 
 export default router;
