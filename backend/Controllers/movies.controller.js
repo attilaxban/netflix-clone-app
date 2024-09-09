d28149ff7bfc6d1842e69c9b8d42cb963c89ad68
Attila Bán <144130755+attilaxban@users.noreply.github.com>
2024-09-09 15:59:23 +0200

    Update movies.controller.js

--- backend/Controllers/movies.controller.js
+++ backend/Controllers/movies.controller.js
@@ -61,15 +61,6 @@ export async function getMoviesByCategory(req, res) {
 	}
 }
 
-<<<<<<< HEAD
-export async function getMoviesByGenre(req,res) {
-	const genreID = req.params.genreID;
-	try {
-		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/discover/movie?api_key=${ENV_VARS.TMDB_API_KEY}&with_genres=${genreID}`)
-		res.status(200).json({ success: true, content: data.results });
-	} catch (error) {
-		res.status(500).json({ success: false, message: "Internal Server Error" });
-=======
 export async function getMoviesByGenres(req,res) {
 	const genreID = req.params.genreID;
 	try {
@@ -78,6 +69,5 @@ export async function getMoviesByGenres(req,res) {
 	} catch (error) {
 	  console.error("Error fetching TV by genre:", error);
 	  res.status(500).json({ success: false, message: "Internal Server Error" });
->>>>>>> frontend
 	}
 }
