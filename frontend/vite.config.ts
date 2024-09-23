9f80a37a23b41787751f6cb6378a8867d5779457
attilaxban <banxattila@gmail.com>
2024-09-23 09:59:26 +0200

    testCommit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://backend:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
         //rewrite: path => path.replace('/api', ''),
